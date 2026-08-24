0761c84c64aa49772b0bbde78918c360cce3a8da
Abel-Lr <113478242+Abel-Lr@users.noreply.github.com>
2026-08-24 22:33:01 +0200

    init: loginPage test

---
+++ fixtures/accounts.ts
@@ -0,0 +1,21 @@
+export interface Account {
+    id: string;
+    username: string;
+    password: string;
+}
+
+export const standardUser: Account = {id: 'standard', username: 'standard_user', password: 'secret_sauce'};
+export const lockedOutUser: Account = { id: 'lockedOut', username: 'locked_out_user', password: 'secret_sauce' };
+export const problemUser: Account = { id: 'problem', username: 'problem_user', password: 'secret_sauce' };
+export const performanceGlitchUser: Account = { id: 'performanceGlitch', username: 'performance_glitch_user', password: 'secret_sauce' };
+export const errorUser: Account = { id: 'error', username: 'error_user', password: 'secret_sauce' };
+export const visualUser: Account = { id: 'visual', username: 'visual_user', password: 'secret_sauce' };
+
+export const accounts: Account[] = [
+    standardUser,
+    lockedOutUser,
+    problemUser,
+    performanceGlitchUser,
+    errorUser,
+    visualUser,
+];
---
+++ tests/login.spec.ts
@@ -0,0 +1,41 @@
+import {test, expect} from "@playwright/test";
+import {LoginPage} from "../pages/LoginPage";
+import {accounts, lockedOutUser} from "../fixtures/accounts";
+
+const unlockedAccounts = accounts.filter(acc => acc.id != 'lockedOut');
+
+test.describe('Authentification réussie', () => {
+    for (const account of unlockedAccounts) {
+        test(`Connexion avec le compte ${account.id}`, async ({page}) => {
+            const loginPage = new LoginPage(page);
+            await loginPage.goto();
+            await loginPage.login(account.username, account.password);
+
+            const cookies = await page.context().cookies();
+            const authCookie = cookies.find(c => c.name === 'session-username');
+
+            expect(authCookie).toBeDefined();
+            expect(authCookie?.value).toBe(account.username);
+        });
+    }
+});
+
+test.describe('Authentification échouée (compte bloqué)', () => {
+    test(`Connexion avec le compte ${lockedOutUser.id}`, async ({page}) => {
+        test.info().annotations.push({
+            type: 'note',
+            description: 'BUG connu : le cookie session-username est créé malgré le blocage du compte'
+        })
+        const loginPage = new LoginPage(page);
+        await loginPage.goto();
+        await loginPage.login(lockedOutUser.username, lockedOutUser.password);
+
+        const errorMessage = await loginPage.getErrorMessage();
+
+        const cookies = await page.context().cookies();
+        const authCookie = cookies.find(c => c.name === 'session-username');
+
+        expect(authCookie).toBeUndefined();
+        expect(errorMessage).toMatch('this user has been locked out')
+    });
+});
